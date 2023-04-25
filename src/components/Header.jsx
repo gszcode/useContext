57cd6118de479ebe92b82762ba26942d2eb1b6c7
Gabriel <gszcode@gmail.com>
2023-04-24 23:27:25 -0300

    texto cambiado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({ toggleTheme }) => {
       <nav className="Nav">
         <h1 className="Logo">My App</h1>
         <button className="Button" onClick={toggleTheme}>
-          {theme === 'ligth' ? 'dark' : 'ligth'} mode
+          {theme === 'ligth' ? 'dark' : 'ligth'} Mode
         </button>
       </nav>
     </header>
